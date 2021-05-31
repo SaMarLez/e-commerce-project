d24602e747d21482d47de146bba86a0cdb95f4cc
SaMarLez <77709957+SaMarLez@users.noreply.github.com>
2021-05-30 19:29:39 -0500

    Base de datos agregada

---
+++ VoltShop/VoltShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VoltShop.Startup))]
+namespace VoltShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
